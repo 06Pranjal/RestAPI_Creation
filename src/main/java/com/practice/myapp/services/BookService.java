a63eba86c530757a506b3efeeb6bb9f35642279c
Pranjal Singh <pranjalsingh0605@gmail.com>
2025-08-10 18:54:02 +0530

    Get mapping from service and Postman

---
+++ src/main/java/com/practice/myapp/services/BookService.java
@@ -0,0 +1,32 @@
+package com.practice.myapp.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.practice.myapp.Entities.Book;
+
+@Component
+public class BookService {
+    private static List<Book> list=new ArrayList<>();
+
+    static{
+        list.add(new Book(12,"Spring Reference","Prakash"));
+        list.add(new Book(14,"DSA","Aakash"));
+        list.add(new Book(15,"OOPS","Vikas"));
+    }
+
+    //get all books
+    public List<Book> getAllBooks(){
+        return list;
+    }
+
+    //get single book
+    public Book getBookById(int id){
+        Book book=null;
+        //stream line function
+        book=list.stream().filter(e->e.getId()==id).findFirst().get();
+        return book;
+    }
+}
