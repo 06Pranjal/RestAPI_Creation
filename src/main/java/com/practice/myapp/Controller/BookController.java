1ba7009939c40a7457d5e15bfff559587c46af4e
Pranjal Singh <pranjalsingh0605@gmail.com>
2025-08-20 13:07:31 +0530

    Response entity

--- src/main/java/com/practice/myapp/Controller/BookController.java
+++ src/main/java/com/practice/myapp/Controller/BookController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,17 +44,30 @@ public class BookController {
     }
 
     @PostMapping("/books")
-    public Book addBook(@RequestBody Book book){
-        Book b=this.bookService.addBook(book);
+    public ResponseEntity<Book> addBook(@RequestBody Book book){
+        Book b=null;
+        try{
+            b=this.bookService.addBook(book);
         System.out.println(book);
-        return b;
+        return ResponseEntity.of(Optional.of(b));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
     }
 
     //delete book
     @DeleteMapping("/books/{bookId}")
-    public void deleteBook(@PathVariable("bookId") int bookId){
-        this.bookService.deleteBook(bookId);
-        System.out.println("Book deleted Succesfully");
+    public ResponseEntity<Void> deleteBook(@PathVariable("bookId") int bookId){
+        try {
+            this.bookService.deleteBook(bookId);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+        
+
     }
 
     //update book handler
